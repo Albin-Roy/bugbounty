ccec6b691997946d430bb17472e9db31bc05ac2a
Albin-Roy <albinroy18@outlook.com>
2022-05-13 01:22:04 -0400

    updated to autorecon.sh

--- 403bypasser.sh
+++ 403bypasser.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-awk '$0="http://"$0' /root/bugbounty/targets/$1/rootDomains/all-subdomains.txt > /root/bugbounty/targets/$1/rootDomains/httpcheck.txt
+awk '$0="http://"$0' /root/bugbounty/targets/$1/root_domains/all-subdomains.txt > /root/bugbounty/targets/$1/root_domains/httpcheck.txt
 echo "Checking http status codes using medic"
-medic /root/bugbounty/targets/$1/rootDomains/httpcheck.txt >> /root/bugbounty/targets/$1/rootDomains/statuscodes.txt
-cat /root/bugbounty/targets/$1/rootDomains/statuscodes.txt | grep "403 " >> /root/bugbounty/targets/$1/rootDomains/403and401.txt
-cat /root/bugbounty/targets/$1/rootDomains/statuscodes.txt | grep "401 " >> /root/bugbounty/targets/$1/rootDomains/403and401.txt
-awk -F "http" '{print "http"$2}' /root/bugbounty/targets/$1/rootDomains/403and401.txt | sort -u > /root/bugbounty/targets/$1/rootDomains/forbidden.txt
+medic /root/bugbounty/targets/$1/root_domains/httpcheck.txt >> /root/bugbounty/targets/$1/root_domains/statuscodes.txt
+cat /root/bugbounty/targets/$1/root_domains/statuscodes.txt | grep "403 " >> /root/bugbounty/targets/$1/root_domains/403and401.txt
+cat /root/bugbounty/targets/$1/root_domains/statuscodes.txt | grep "401 " >> /root/bugbounty/targets/$1/root_domains/403and401.txt
+awk -F "http" '{print "http"$2}' /root/bugbounty/targets/$1/root_domains/403and401.txt | sort -u > /root/bugbounty/targets/$1/root_domains/forbidden.txt
 
 arr=()
 while IFS= read -r line; do
    arr+=("$line")
-done </root/bugbounty/targets/$1/rootDomains/forbidden.txt
+done </root/bugbounty/targets/$1/root_domains/forbidden.txt
 
 indexes=( "${!arr[@]}" )
 lastindex=${indexes[-1]}
@@ -20,10 +20,10 @@ count=1
 for i in "${arr[@]}"
 do
 	echo ""
-	echo "$i" >> /root/bugbounty/targets/$1/rootDomains/403bypass.txt
+	echo "$i" >> /root/bugbounty/targets/$1/root_domains/403bypass.txt
 	echo -e "Scanning $i           [ $count/$lastindex ] "
-	byp4xx.py -L $i >> /root/bugbounty/targets/$1/rootDomains/403bypass.txt
-	echo -e "\n\n" >> /root/bugbounty/targets/$1/rootDomains/403bypass.txt
+	byp4xx.py -L $i >> /root/bugbounty/targets/$1/root_domains/403bypass.txt
+	echo -e "\n\n" >> /root/bugbounty/targets/$1/root_domains/403bypass.txt
 	count=`expr $count + 1`
 done
 echo "All subdomains scanned..!"
--- AllSub.sh
+++ AllSub.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-find /root/bugbounty/targets/$1/rootDomains/ -type f -name "ALIVE.txt" > /root/bugbounty/targets/$1/rootDomains/all-sub-path.txt
+#collects all alive subdomains and combine it into a single file
+
+find /root/bugbounty/targets/$1/root_domains/ -type f -name "ALIVE.txt" > /root/bugbounty/targets/$1/root_domains/all-sub-path.txt
 sleep 5
 arr=()
 while IFS= read -r line; do
    arr+=("$line")
-done </root/bugbounty/targets/$1/rootDomains/all-sub-path.txt
+done </root/bugbounty/targets/$1/root_domains/all-sub-path.txt
 
 indexes=( "${!arr[@]}" )
 lastindex=${indexes[-1]}
@@ -14,6 +16,6 @@ count=1
 
 for i in "${arr[@]}"
 do
-	cat $i >> /root/bugbounty/targets/$1/rootDomains/all-subdomains.txt
+	cat $i >> /root/bugbounty/targets/$1/root_domains/all-subdomains.txt
 done
 echo "All collected subdomains saved..!"
